10d534198080df0aed354654a2ac34b8c849eb15
Martin Ottersland <ottin93@gmail.com>
2024-09-09 19:20:45 +0200

    Bump version to 1.0.18 and disable custom element in svelte config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    svelte(),
+    svelte({
+      compilerOptions: {
+        customElement: false,
+      },
+    }),
     dts({
       include: ["./src/lib"],
       exclude: ["**/*.spec.ts", "**/*.test.ts"],
