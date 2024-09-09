b0cfc1dcd75254c6bf06a3bf9be9eb9b02a57d21
Martin Ottersland <ottin93@gmail.com>
2024-09-09 18:27:34 +0200

    Bump version to 1.0.12 and update vite.config.ts for component bundling

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,41 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import {defineConfig} from "vite";
+import {svelte} from "@sveltejs/vite-plugin-svelte";
 import path from "path";
 import dts from "vite-plugin-dts";
+import pkg from './package.json';
+
+const bundleComponents = process.env.BUNDLE_COMPONENTS ?? true;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    svelte({
-      compilerOptions: {
-        customElement: false,
-      },
-    }),
-    dts({
-      include: ["src/lib/*.ts"],
-    }),
-  ],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/lib/index.ts"),
-      name: "sly-svelte-pane",
-      fileName: (format) => `sly-svelte-pane.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['svelte'],
-      output: {
-        globals: {
-          svelte: "Svelte",
+    plugins: [
+        svelte({
+            compilerOptions: {
+                customElement: false,
+            },
+        }),
+        dts({
+            include: ["src/lib/*.ts"],
+        }),
+    ],
+    build: {
+        emptyOutDir: true,
+        lib: {
+            entry: path.resolve(__dirname, "src/lib/index.ts"),
+            formats: bundleComponents ? ['es', 'esm', 'umd'] as any : ['es'],
+            name: pkg.name,
+            fileName: (format) => `${pkg.name}.${format}.js`
         },
-      },
+        rollupOptions: {
+            external: ['svelte'],
+            output: bundleComponents ? {} : {
+                inlineDynamicImports: false,
+                chunkFileNames: "[name].js",
+                manualChunks: {'svelte': ["svelte"]},
+                globals: {
+                  svelte: "Svelte",
+                }
+            }
+        },
     },
-  },
 });
