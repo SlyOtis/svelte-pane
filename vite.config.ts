76962c6f9772d550d0b45cf43993af9b4ba1593f
Martin Ottersland <m@slytech.no>
2024-09-09 18:58:20 +0200

    Update package version to 1.0.14 and adjust Vite config for ES format only

--- vite.config.ts
+++ vite.config.ts
@@ -2,38 +2,25 @@ import {defineConfig} from "vite";
 import {svelte} from "@sveltejs/vite-plugin-svelte";
 import path from "path";
 import dts from "vite-plugin-dts";
-import pkg from './package.json';
 
 const bundleComponents = process.env.BUNDLE_COMPONENTS ?? true;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        svelte({
-            compilerOptions: {
-                customElement: false,
-            },
-        }),
-        dts({
-            include: ["src/lib/*.ts"],
-        }),
-    ],
+    plugins: [svelte()],
     build: {
         emptyOutDir: true,
         lib: {
             entry: path.resolve(__dirname, "src/lib/index.ts"),
-            formats: bundleComponents ? ['es', 'esm', 'umd'] as any : ['es'],
-            name: pkg.name,
-            fileName: (format) => `${pkg.name}.${format}.js`
+            name: "sly-svelte-pane",
+            fileName: 'index',
+            formats: ['es']
         },
         rollupOptions: {
             external: ['svelte'],
-            output: bundleComponents ? {} : {
-                inlineDynamicImports: false,
-                chunkFileNames: "[name].js",
-                manualChunks: {'svelte': ["svelte"]},
+            output: {
                 globals: {
-                  svelte: "Svelte",
+                    svelte: 'Svelte'
                 }
             }
         },
