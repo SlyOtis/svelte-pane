233292d2c59c13efeba9c4eae0672060cfcde803
Martin Ottersland <m@slytech.no>
2024-09-09 21:24:18 +0200

    Bump version to 1.0.31 and update vite config for dts plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,29 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import {defineConfig} from "vite";
+import {svelte} from "@sveltejs/vite-plugin-svelte";
 import path from "path";
 import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts({ rollupTypes: true }), svelte()],
-  build: {
-    sourcemap: true,
-    lib: {
-      entry: path.resolve(__dirname, "src/lib/index.ts"),
-      name: "SlySveltePane",
-      formats: ["es", "cjs", "umd", "iife"],
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["svelte"],
-      output: {
-        globals: {
-          react: "Svelte",
+    plugins: [dts({
+        include: ["./src/lib"],
+        rollupTypes: true
+    }), svelte()],
+    build: {
+        sourcemap: true,
+        lib: {
+            entry: path.resolve(__dirname, "src/lib/index.ts"),
+            name: "SlySveltePane",
+            formats: ["es", "cjs", "umd", "iife"],
+            fileName: (format) => `index.${format}.js`,
         },
-      },
+        rollupOptions: {
+            external: ["svelte"],
+            output: {
+                globals: {
+                    react: "Svelte",
+                },
+            },
+        },
     },
-  },
 });
