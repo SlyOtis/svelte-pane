fb8170e1000e6f94985a72ba4187ae7e4b1b8cc9
Martin Ottersland <ottin93@gmail.com>
2024-09-20 17:09:10 +0200

    Add CellValue component and refactor FileTreeItem to use it

---
+++ src/lib/CellValue.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let key: string
+    export let value: string
+
+    $: {
+
+    }
+</script>
+
+<li class="metadata-{key} grid">
+    <span class="value">{value}</span>
+</li>
